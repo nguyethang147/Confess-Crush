b708b2e24f1adc883cb92aaff92334b5d4a29d6d
nguyethang147 <94798068+nguyethang147@users.noreply.github.com>
2021-11-21 23:51:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "anh iuuuuuu",
+    introTitle: 'Tran Viet Thanh',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    title: 'Phải chăng anh iu đã yêu em ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Em iu thuỳ mị nết na giỏi giang lại còn xink đẹp thì phải iu chứuu',
+    btnNo: 'mơ à, ai bảo iu cơ',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao anh iu lại yêu em thế nhờoo',
+    btnReply: 'Gửi ik gửi ik nèo b ngủ quá rùi',
+    reply: 'Yêu khum cần lý dooooooo',
+    mess: 'Nma ý, NH iu TTr nhìu hơn cơ ý, iu nhắm nên là mỗi ngày, sao lại ko trao những lời iu thương cho nhữn ngừi mìn iu được nhỉ, hum nai NH sẽ iu TTr nhìu hơn hum qua mụt chút và như thế cứ nhân nhân tìn iu của mìn lên thui, đừng làm NH nổi nóng là đc ạ, iu TTr nhiều nhắm cơ 😘😘',
+    messDesc: 'Khi nãy mìn bùn cừi lắm =)) tại mìn ko biết hôn đến bao giờ mới xong ý =)) nma hok xao mìn bù mấy buổi sáng nhìu người cũn đc =)))) Đọc xong rùi thì còn chần chừ gì nữa mà ko thơm (online) tớ một cái nhỉ =)))))',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
